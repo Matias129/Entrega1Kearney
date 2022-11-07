90152102bb47bfcc114780729ec98669fde8bc89
Matias129 <117055574+Matias129@users.noreply.github.com>
2022-11-07 18:52:02 -0300

    Miprimercommit

---
+++ test_coder/test_coder/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.template import Template, context
+
+from datetime import datetime
+
+
+
+def fecha_actual (request):
+    hoy = datetime.now().strftime("%Y/%m/%d")
+
+    return HttpResponse (f"Fecha actual: {hoy}")
+
+
+def inicio(request):
+    
+    archivo = open(r"/Users/TT/Documents/AC_DJANGO/test_coder/test_coder/templates/inicio.html"   , "r")
+    
+
+    plantilla = Template (archivo.read)
+
+    archivo.close()
+
+    contexto = context()
+
+    documento = plantilla.render(contexto)
+
+    return HttpResponse(documento)
+
