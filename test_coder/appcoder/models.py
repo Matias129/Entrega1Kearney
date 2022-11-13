50335e22c2fa9eac54d203f37627e0c53e11b1f8
Matias129 <117055574+Matias129@users.noreply.github.com>
2022-11-13 13:33:37 -0300

    agregamos modelos

--- test_coder/appcoder/models.py
+++ test_coder/appcoder/models.py
@@ -2,6 +2,27 @@ from django.db import models
 
 # Create your models here.
 
+
+class Modelos (models.Model):
+
+    modelo = models.CharField (max_length=50)
+    procedencia = models.CharField (max_length=50)
+    patente = models.IntegerField ()
+
+class Marcas (models.Model):
+
+    modelo = models.CharField (max_length=50)
+    procedencia = models.CharField (max_length=50)
+    patente = models.IntegerField ()
+
+class Peticiones_vent (models.Model):
+
+    nombre = models.CharField (max_length=50)
+    modelo = models.CharField (max_length=50)
+    Fecha_de_venta = models.DateField ()
+
+
+
 class Seccion_ingreso (models.Model):
 
     nombre = models.CharField (max_length=50)
